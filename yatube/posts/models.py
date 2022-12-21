943b6a0aa22f68ce14677ec66a7bebd8d113c29d
Александр Сакулин <sakulisakulin@yandex.ru>
2022-12-21 16:23:47 +0300

    homework_version2

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -1,30 +1,51 @@
-from django.db import models
 from django.contrib.auth import get_user_model
+from django.db import models
+
 
 User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200)
-    slug = models.SlugField(unique=True)
-    description = models.TextField()
+    title = models.CharField(
+        verbose_name='Название группы',
+        max_length=200
+    )
+    slug = models.SlugField(
+        verbose_name='Слаг группы',
+        unique=True
+    )
+    description = models.TextField(
+        verbose_name='Описание группы'
+    )
 
     def __str__(self):
         return self.title
 
 
 class Post(models.Model):
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(
+        verbose_name='Пост'
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата',
+        auto_now_add=True
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
-        on_delete=models.CASCADE,
-        related_name='posts'
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        verbose_name='Группа'
     )
+    def __str__(self):
+        return self.text[:30]
+
+    class Meta:
+        ordering = ['-pub_date']
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,22 +1,23 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Post, Group
+from django.shortcuts import get_object_or_404, render
+
+from django.conf import settings
+
+from .models import Group, Post
 
 
 def index(request):
-    template = 'posts/index.html'
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.all()[:settings.POSTS_IN_PAGE]
     context = {
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = group.posts.all()[:settings.POSTS_IN_PAGE]
     context = {
         'group': group,
         'posts': posts,
     }
-    return render(request, template, context)
+    return render(request, 'posts/group_list.html', context)
