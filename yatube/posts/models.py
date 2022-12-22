d87c666a907d149a16a097101139f21204eeb668
Александр Сакулин <sakulisakulin@yandex.ru>
2022-12-22 19:43:47 +0300

    homework_ver_4

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -7,8 +7,9 @@ User = get_user_model()
 
 class Group(models.Model):
     title = models.CharField(
-        max_length=200,
         verbose_name='Название группы',
+        help_text='Выберите группу для поста',
+        max_length=200
     )
     slug = models.SlugField(
         verbose_name='Слаг группы',
@@ -50,7 +51,7 @@ class Post(models.Model):
     )
 
     class Meta:
-        ordering = [('-pub_date')]
+        ordering = ('-pub_date',)
 
     def __str__(self):
         return self.text[:30]
